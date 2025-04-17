30a59cabdb749de1c57ce98f17b8ffd99d8ea72a
Mahesh5726 <maheshr070204@gmail.com>
2025-04-17 23:56:44 +0530

    style: refine Footer and NavBar components for improved layout and interactivity
    - Adjusted Footer component structure for better alignment and spacing, enhancing responsiveness.
    - Updated NavBar styles with additional padding and changed the displayed name for consistency.
    - Introduced a new card hover effect component to enhance user interaction with grid items.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,40 +4,38 @@ import { GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
 
 const Footer = () => {
   return (
-    <footer className="border-t">
-      <div className="container flex flex-col sm:flex-row items-center justify-between gap-4 py-4 sm:py-6">
-        <Text
-          size={{ initial: "1", sm: "2" }}
-          color="gray"
-          className="text-center sm:text-left"
-        >
-          Copyright © 2025 Mahesh R. All rights reserved.
-        </Text>
+    <div className="border-t container flex flex-col sm:flex-row items-center justify-between gap-4 py-4 sm:py-6">
+      <Text
+        size={{ initial: "1", sm: "2" }}
+        color="gray"
+        className="text-center sm:text-left"
+      >
+        Copyright © 2025 Mahesh R. All rights reserved.
+      </Text>
 
-        <div className="flex flex-row gap-4">
-          <Link
-            href="https://github.com/Mahesh5726"
-            className="text-foreground/60 hover:text-foreground/80 transition-colors"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div>
-              <GitHubLogoIcon className="w-4 h-4" />
-            </div>
-          </Link>
-          <Link
-            href="https://www.linkedin.com/in/mahesh-r-1ba86123a/"
-            className="text-foreground/60 hover:text-foreground/80 transition-colors"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div>
-              <LinkedInLogoIcon className="w-4 h-4" />
-            </div>
-          </Link>
-        </div>
+      <div className="flex flex-row gap-4">
+        <Link
+          href="https://github.com/Mahesh5726"
+          className="text-foreground/60 hover:text-foreground/80 transition-colors"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div>
+            <GitHubLogoIcon className="w-6 h-6" />
+          </div>
+        </Link>
+        <Link
+          href="https://www.linkedin.com/in/mahesh-r-1ba86123a/"
+          className="text-foreground/60 hover:text-foreground/80 transition-colors"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="rounded-full bg-violet-300/5 hover:bg-violet-300/10 transition-colors">
+            <LinkedInLogoIcon className="w-6 h-6" />
+          </div>
+        </Link>
       </div>
-    </footer>
+    </div>
   );
 };
 
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -23,7 +23,7 @@ const NavBar = (props: { className?: string }) => {
   return (
     <div
       className={
-        "nav-container mx-auto border-b border-foreground/10 pb-4" +
+        "nav-container mx-auto border-b border-foreground/10 pb-4 p-2" +
         (props.className ? props.className : "")
       }
     >
@@ -35,7 +35,7 @@ const NavBar = (props: { className?: string }) => {
               weight="bold"
               className="text-foreground"
             >
-              Mahesh R
+              M57
             </Heading>
             <span className="absolute left-0 bottom-0 h-[2px] w-0 bg-current transition-all duration-300 group-hover:w-full" />
           </Link>
