63742b2df87f36054a9c475a15612048a291a06b
Mahesh5726 <maheshr070204@gmail.com>
2025-04-17 23:57:25 +0530

    feat: enhance Projects page layout with NavBar and Footer components
    - Updated the Projects page to include a NavBar and Footer for consistent navigation and improved layout.
    - Structured the page with a flex container to enhance responsiveness and visual organization.

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,9 +1,21 @@
+import Footer from '@/components/Footer';
+import NavBar from '@/components/NavBar';
 import React from 'react'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
+    <div className="flex min-h-screen flex-col">
+      <div className="container mx-auto py-6 sm:py-10">
+        <NavBar />
+      </div>
+
+      <div></div>
+
+      <div className="container mx-auto mt-auto">
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
 export default Projects
